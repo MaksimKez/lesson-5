57dcf1c7f77c6de1fbd39cdef20dbb9ecb87687b
MaksimKez <prostomaxim765@gmail.com>
2024-02-05 02:29:20 +0300

    few steps

--- lesson 5/Program.cs
+++ lesson 5/Program.cs
@@ -44,34 +44,34 @@ namespace lesson_5
                     switch (c)
                     {
                         case '0':
-                            result.Append("ноль");
+                            result.Append("ноль ");
                             break;
                         case '1':
-                            result.Append("один");
+                            result.Append("один ");
                             break;
                         case '2':
-                            result.Append("два");
+                            result.Append("два ");
                             break;
                         case '3':
-                            result.Append("три");
+                            result.Append("три ");
                             break;
                         case '4':
-                            result.Append("четыре");
+                            result.Append("четыре ");
                             break;
                         case '5':
-                            result.Append("пять");
+                            result.Append("пять ");
                             break;
                         case '6':
-                            result.Append("шесть");
+                            result.Append("шесть ");
                             break;
                         case '7':
-                            result.Append("семь");
+                            result.Append("семь ");
                             break;
                         case '8':
-                            result.Append("восемь");
+                            result.Append("восемь ");
                             break;
                         case '9':
-                            result.Append("девять");
+                            result.Append("девять ");
                             break;
                         default:
                             result.Append(c);
@@ -87,13 +87,80 @@ namespace lesson_5
             return result.ToString();
         }
 
+        static void Intonation(string inputStr)
+        {
+            int lastSignIndex = 0;
+            int counter = 0;
+            StringBuilder newStrQu = new StringBuilder("");
+            StringBuilder newStrEx = new StringBuilder("");
+
+            foreach (var c in inputStr)
+            {
+                switch (c)
+                {
+                    case '?':
+                        newStrQu.Append(inputStr.Substring(lastSignIndex, counter));
+                        lastSignIndex = counter;
+                        counter++;
+                        break;
+                    case '!':
+                        newStrEx.Append(inputStr.Substring(lastSignIndex, counter));
+                        lastSignIndex = counter;
+                        counter++;
+                        break;
+                    case '\n':
+                    case '\r':
+                        newStrEx.Append(inputStr.Substring(lastSignIndex, counter));
+                        break;
+                    default: counter++; break;
+                }
+
+
+
+                //if (c == '?')
+                //{
+                //    newStrQu.Append(inputStr.Substring(lastSignIndex, counter));
+                //    lastSignIndex = counter;                    
+                //}
+                //else if (c == '!')
+                //{
+                //    newStrEx.Append(inputStr.Substring(lastSignIndex, counter));
+                //    lastSignIndex = counter;
+                //}
+            }
+
+            //for (int i = 0; i < inputStr.Length; i++)
+            //{
+            //    if (inputStr[i] == '\r' || inputStr[i] == '\n') { i--; continue; }
+            //    if (inputStr[i] == '?')
+            //    {
+            //        newStrQu.Append(inputStr.Substring(lastSignIndex, i - 1));
+            //        lastSignIndex = i;
+            //    }
+            //    else if (inputStr[i] == '!')
+            //    {
+            //        newStrEx.Append(inputStr.Substring(lastSignIndex, i - 1));
+            //        lastSignIndex = i;
+            //    }
+            //}
+
+            Console.WriteLine(newStrQu.ToString());
+            Console.WriteLine(newStrEx.ToString());
+        } //todo что?
+
+        static void SameLetters(string inputStr)
+        {
+
+        }
+
 // todo - Вывести на экран сначала вопросительные, а затем восклицательные предложения.
 // todo - Вывести на экран только предложения, не содержащие запятых.
 // todo - Найти слова, начинающиеся и заканчивающиеся на одну и ту же букву.
 
+
         static void Main(string[] args)
         {
-            string line;
+            string line = "";
             string path = "";
             StreamReader sr;
             while (true)
@@ -102,6 +169,9 @@ namespace lesson_5
                 {
                     path = Console.ReadLine();
                     sr = new StreamReader(path);
+                    //if (sr.ReadLine() == null) Console.WriteLine("err"); break;
+                    line = sr.ReadToEnd();
+                    Console.WriteLine("Ваш текст: \n"  + line);
                     break;
                 }
                 catch (Exception e)
@@ -110,9 +180,9 @@ namespace lesson_5
                     continue;
                 }
             }
-            Console.WriteLine("дктидылпиотыдпи");
-            line = sr.ReadLine();
-            Console.WriteLine(line);
+            //todo выбор действия
+            Intonation(line);
         }
     }
 }
+//? ?????? Поместить в список все строки, затем разделить на условные предложения по индексу(?) 
